b042b7756c957426ff729151332e1c5295acdf71
football3o <63215568+football3o@users.noreply.github.com>
2021-03-06 16:40:49 -0800

    Update spike_trap.h

--- spike_trap.h
+++ spike_trap.h
@@ -20,7 +20,7 @@ class Spike_Trap: public Trap{
 			if(chance < 50){
 				cout << "Spike Trap was not successfully disarmed. You took damage and are not bleeding" << endl;
 				p1->set_health(p1->get_health() - damage);
-				p1->set_status(getStatus());
+				p1->status = getStatus();
 				p1->AddItems(getItem());
 			}else{
 				cout << "Spike Trap was successfully disarmed" <<endl;

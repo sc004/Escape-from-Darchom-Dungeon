0e9f63f5cf6fcfba77767e03b77720a68aee1f54
football3o <63215568+football3o@users.noreply.github.com>
2021-03-07 11:26:24 -0800

    Create enemyencounterfactory.h

---
+++ enemyencounterfactory.h
@@ -0,0 +1,19 @@
+#ifndef ENEMYENCOUNTERFACTORY_H
+#define ENEMYENCOUNTERFACTORY_H
+
+#include <iostream>
+#include <string>
+#include "encounterfactory.h"
+#include "enemyencounter.h"
+
+using namespace std;
+class EnemyEncounterFactory: public EncounterFactory{
+	public:
+		EnemyEncounterFactory(){}
+		~EnemyEncounterFactory();
+		virtual Encounter* makeEncounter(){
+			Encounter* enemy = new EnemyEncounter(1);
+			return enemy;
+		}
+};
+#endif //ENEMYENCOUNTERFACTORY_H

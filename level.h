241ae3e45449a6a0598334532c2539bd712c2005
football3o <63215568+football3o@users.noreply.github.com>
2021-03-07 12:16:24 -0800

    Updated levels to reflect what is used and added includes

--- level.h
+++ level.h
@@ -1,11 +1,17 @@
+#ifndef LEVEL_H
+#define LEVEL_H
 
+#include "encounter.h"
+#include <string>
+#include <iostream>
 
+using namespace std;
 class Level{
 	private:
 	int <Encounter*> encounters;
 	string name;	
 	public:
-	void getLevel(Level* l){
-	
-	}
+	Level();
+	~Level();
 };
+#endif //LEVEL_H
